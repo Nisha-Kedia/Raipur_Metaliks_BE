61dd74b642d619c8d7ff2175ed0fda2546dcd96d
Nisha-Kedia <114925494+Nisha-Kedia@users.noreply.github.com>
2025-07-22 01:39:19 +0530

    Update WebConfig.java

--- src/main/java/Raipur/Metaliks/example/Raipur/Metaliks/Config/WebConfig.java
+++ src/main/java/Raipur/Metaliks/example/Raipur/Metaliks/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://raipurmetaliksfe-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
