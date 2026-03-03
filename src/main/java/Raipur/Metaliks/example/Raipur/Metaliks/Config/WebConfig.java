1c2d5c2531db81e6f44bd5caa6de08d48e51f19b
raj-alpha-swain <rajalphaswain786@gmail.com>
2026-03-03 17:12:05 +0530

    changes in prod

--- src/main/java/Raipur/Metaliks/example/Raipur/Metaliks/Config/WebConfig.java
+++ src/main/java/Raipur/Metaliks/example/Raipur/Metaliks/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package Raipur.Metaliks.example.Raipur.Metaliks.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://raipurmetaliksfe-production.up.railway.app:8080")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
